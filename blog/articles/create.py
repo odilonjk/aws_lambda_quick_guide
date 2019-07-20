3f647a05db42926ccd4f3b0f3c56b2a7d872db96
OdilonJK <kroger.odilonj@gmail.com>
2019-07-20 20:37:52 -0300

    Integrated create_article and DynamoDB

--- blog/articles/create.py
+++ blog/articles/create.py
@@ -1,15 +1,48 @@
+from __future__ import print_function
 import json
+import boto3
+import decimal
+import uuid
+
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if abs(o) % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
 
 
 def create_article(event, context):
+    title = event["title"]
     text = event["text"]
+
+    validate_text(title)
+    validate_text(text)
+
+    dynamodb = boto3.client("dynamodb")
+
+    response = dynamodb.put_item(
+        TableName="BlogTable",
+        Item={
+            "article_id": {"S": str(uuid.uuid4())},
+            "title": {"S": title},
+            "text": {"S": text}
+        }
+    )
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(response, indent=4, cls=DecimalEncoder)
+    }
+
+    return response
+
+
+def validate_text(text):
     if not isinstance(text, str):
         raise TypeError("Text property should be of text type.")
     if not text.strip():
         raise ValueError("Text property should not be empty.")
-    response = {
-        "statusCode": 200,
-        "body": json.dumps({"message": "Article created with success!"})
-    }
-
-    return response
